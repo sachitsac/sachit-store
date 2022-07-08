626e95eed6e82ab64a9c17b1567fa600867626f4
Sachit <sachitsac@gmail.com>
2022-07-08 15:53:42 +1000

    feat: added a dummy products datastore

---
+++ src/data/products.ts
@@ -0,0 +1,28 @@
+import type { Product } from '../models/Product';
+export const products = (): Product[] => [
+  {
+    sku: 'ipd',
+    name: 'Super iPad',
+    price: 54999,
+  },
+  {
+    sku: 'mbp',
+    name: 'MacBook Pro',
+    price: 139999,
+  },
+  {
+    sku: 'atv',
+    name: 'Apple TV',
+    price: 10950,
+  },
+  {
+    sku: 'ipd',
+    name: 'Super iPad',
+    price: 54999,
+  },
+  {
+    sku: 'vga',
+    name: 'VGA adapter',
+    price: 3000,
+  },
+];
