4b8a1bcc3c28da4552a02fa23a3da654d91bc031
Sachit <sachitsac@gmail.com>
2022-07-08 15:54:04 +1000

    fix: product interface

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,9 +1,5 @@
-import { z } from 'zod';
-
-export const ProductSchema = z.object({
-  sku: z.string(),
-  name: z.string(),
-  price: z.number(),
-});
-
-export type Product = z.infer<typeof ProductSchema>;
+export interface Product {
+  sku: string;
+  name: string;
+  price: number;
+}
