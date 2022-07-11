22e663e6056890be75d52d2d9c8445cc080b97aa
Sachit <sachitsac@gmail.com>
2022-07-11 13:20:49 +1000

    chore: added missing tests

--- tests/repositories/ProductConfigurationRepository.spec.ts
+++ tests/repositories/ProductConfigurationRepository.spec.ts
@@ -24,4 +24,29 @@ describe('Product Configuration Repository', () => {
     const productConfig = productConfigRepository.findBySku('test');
     expect(productConfig).toBeDefined();
   });
+
+  it('should return undefined if no product configuration found', () => {
+    const productConfigMock = jest.spyOn(productConficuration, 'productConficuration');
+    productConfigMock.mockReturnValue([]);
+    const productConfigRepository = new ProductConfigurationRepository();
+    expect(productConfigRepository.getProductConfiguration().length).toEqual(0);
+  });
+
+  it('should find all product configuration', () => {
+    const productsMock = jest.spyOn(productConficuration, 'productConficuration');
+    productsMock.mockReturnValue([
+      {
+        skus: ['test'],
+        strategy: 'bulk',
+        config: { price: 10 },
+      },
+      {
+        skus: ['test2'],
+        strategy: 'quantity',
+        config: { price: 10 },
+      },
+    ]);
+    const productConfigRepository = new ProductConfigurationRepository();
+    expect(productConfigRepository.getProductConfiguration().length).toEqual(2);
+  });
 });
