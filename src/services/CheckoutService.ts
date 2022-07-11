3eead994101b0a57857e02b1f74095afb24e83e0
Sachit <sachitsac@gmail.com>
2022-07-11 22:43:29 +1000

    feat: quantity discount calculation

--- src/services/CheckoutService.ts
+++ src/services/CheckoutService.ts
@@ -1,11 +1,16 @@
 import type { ICheckoutService } from './ICheckoutService';
+import type { IProductConfigurationRepository } from '../repositories/IProductConfigurationRepository';
 import type { IProductRepository } from '../repositories/IProductRepository';
 import type { Product } from './../models/Product';
+import type { ProductConfiguration } from '../models/ProductConfiguration';
 
 export class CheckoutService implements ICheckoutService {
   public items: Product[] = [];
 
-  public constructor(private readonly productRepo: IProductRepository) {}
+  public constructor(
+    private readonly productRepo: IProductRepository,
+    private readonly productConficurationRepository: IProductConfigurationRepository,
+  ) {}
 
   public scan(sku: Product['sku']): void {
     const product = this.productRepo.findProductBySku(sku);
@@ -16,6 +21,40 @@ export class CheckoutService implements ICheckoutService {
     if (this.items.length === 0) {
       return 0;
     }
-    return this.items.reduce((acc, item) => acc + item.price, 0);
+    const normalPriceTotal = this.items.reduce((acc, item) => acc + item.price, 0);
+    return normalPriceTotal - this.calculateDiscounts();
+  }
+
+  private calculateDiscounts(): number {
+    const itemsBySku = this.groupBy(this.items, (item) => item.sku);
+
+    let discountAmount = 0;
+    itemsBySku.forEach((items) => {
+      const pricingConfig = this.productConficurationRepository.findBySku(items[0].sku);
+      if (pricingConfig != null) {
+        if (pricingConfig.strategy === 'quantity') {
+          discountAmount += this.calculateQuantityDiscount(items, pricingConfig);
+        }
+      }
+    });
+
+    return discountAmount;
+  }
+  private calculateQuantityDiscount(items: Product[], pricingConfig: ProductConfiguration): number {
+    const quantity = items.length;
+    const discount = Math.floor(quantity / Number(pricingConfig.config.quantityThreshold));
+    return discount > 0 ? discount * Number(pricingConfig.config.discountedPrice) : 0;
+  }
+
+  private groupBy<K, V>(array: V[], keyFn: (item: V) => K): Map<K, V[]> {
+    return array.reduce((store, item) => {
+      const key = keyFn(item);
+      if (!store.has(key)) {
+        store.set(key, [item]);
+      } else {
+        store.get(key)?.push(item);
+      }
+      return store;
+    }, new Map<K, V[]>());
   }
 }
--- tests/services/CheckoutService.spec.ts
+++ tests/services/CheckoutService.spec.ts
@@ -1,11 +1,14 @@
 import { instance, mock, reset, verify, when } from 'ts-mockito';
 import { CheckoutService } from '../../src/services/CheckoutService';
 import type { ICheckoutService } from './../../src/services/ICheckoutService';
+import type { IProductConfigurationRepository } from '../../src/repositories/IProductConfigurationRepository';
 import type { IProductRepository } from '../../src/repositories/IProductRepository';
 import type { Product } from './../../src/models/Product';
 
 const mockedProductRepository = mock<IProductRepository>();
-const setup = (): ICheckoutService => new CheckoutService(instance(mockedProductRepository));
+const mockedProductConfigurationRepository = mock<IProductConfigurationRepository>();
+const setup = (): ICheckoutService =>
+  new CheckoutService(instance(mockedProductRepository), instance(mockedProductConfigurationRepository));
 
 describe('Checkout Service', () => {
   beforeEach(() => {
@@ -16,52 +19,172 @@ describe('Checkout Service', () => {
     expect(CheckoutService).toBeDefined();
   });
 
-  it('should scan an item', () => {
-    const item: Product = {
-      name: 'test',
-      price: 1,
-      sku: 'test',
-    };
-    const checkoutService = setup();
-    checkoutService.scan(item.sku);
-    expect(checkoutService.items.length).toEqual(1);
-  });
+  describe('with no discount', () => {
+    it('should scan an item', () => {
+      const item: Product = {
+        name: 'test',
+        price: 1,
+        sku: 'test',
+      };
+      const checkoutService = setup();
+      checkoutService.scan(item.sku);
+      expect(checkoutService.items.length).toEqual(1);
+    });
 
-  it('should return total = 0 if no items are scanned', () => {
-    const checkoutService = setup();
-    expect(checkoutService.total()).toEqual(0);
-  });
+    it('should return total = 0 if no items are scanned', () => {
+      const checkoutService = setup();
+      expect(checkoutService.total()).toEqual(0);
+    });
 
-  it('should throw an error if product cannot be scanned', () => {
-    const checkoutService = setup();
-    when(mockedProductRepository.findProductBySku('test')).thenThrow(new Error('Product with sku test not found'));
-    try {
-      checkoutService.scan('test');
-      verify(mockedProductRepository.findProductBySku('test')).once();
-      fail('product not found');
-    } catch (error: unknown) {
-      expect((error as Error).message).toEqual('Product with sku test not found');
-    }
-  });
+    it('should throw an error if product cannot be scanned', () => {
+      const checkoutService = setup();
+      when(mockedProductRepository.findProductBySku('test')).thenThrow(new Error('Product with sku test not found'));
+      try {
+        checkoutService.scan('test');
+        verify(mockedProductRepository.findProductBySku('test')).once();
+        fail('product not found');
+      } catch (error: unknown) {
+        expect((error as Error).message).toEqual('Product with sku test not found');
+      }
+    });
 
-  it('should return item total', () => {
-    const item1: Product = {
-      name: 'test',
-      price: 10,
-      sku: 'test',
-    };
-    const item2: Product = {
-      name: 'test',
-      price: 15,
-      sku: 'test-2',
-    };
-    when(mockedProductRepository.findProductBySku(item1.sku)).thenReturn(item1);
-    when(mockedProductRepository.findProductBySku(item2.sku)).thenReturn(item2);
-    const checkoutService = setup();
-    checkoutService.scan(item1.sku);
-    checkoutService.scan(item2.sku);
-    verify(mockedProductRepository.findProductBySku(item1.sku)).once();
-    verify(mockedProductRepository.findProductBySku(item2.sku)).once();
-    expect(checkoutService.total()).toEqual(25);
+    it('should return item total', () => {
+      const item1: Product = {
+        name: 'test',
+        price: 10,
+        sku: 'test',
+      };
+      const item2: Product = {
+        name: 'test',
+        price: 15,
+        sku: 'test-2',
+      };
+      when(mockedProductRepository.findProductBySku(item1.sku)).thenReturn(item1);
+      when(mockedProductRepository.findProductBySku(item2.sku)).thenReturn(item2);
+      const checkoutService = setup();
+      checkoutService.scan(item1.sku);
+      checkoutService.scan(item2.sku);
+      verify(mockedProductRepository.findProductBySku(item1.sku)).once();
+      verify(mockedProductRepository.findProductBySku(item2.sku)).once();
+      expect(checkoutService.total()).toEqual(25);
+    });
+
+    describe('with quantity discounts', () => {
+      it('should apply quantity discount', () => {
+        const checkoutService = setup();
+        const item1 = {
+          sku: 'atv',
+          name: 'Apple TV',
+          price: 10950,
+        };
+        const item2 = {
+          sku: 'vga',
+          name: 'VGA adapter',
+          price: 3000,
+        };
+        when(mockedProductRepository.findProductBySku(item1.sku)).thenReturn(item1);
+        when(mockedProductRepository.findProductBySku(item2.sku)).thenReturn(item2);
+        when(mockedProductConfigurationRepository.findBySku(item1.sku)).thenReturn({
+          skus: [item1.sku],
+          strategy: 'quantity',
+          config: {
+            quantityThreshold: 3,
+            discountedPrice: 10950,
+          },
+        });
+        checkoutService.scan(item1.sku);
+        checkoutService.scan(item1.sku);
+        checkoutService.scan(item1.sku);
+        checkoutService.scan(item2.sku);
+        verify(mockedProductRepository.findProductBySku(item1.sku)).times(3);
+        verify(mockedProductRepository.findProductBySku(item2.sku)).once();
+        expect(checkoutService.total()).toEqual(24900);
+      });
+
+      it('should apply quantity discount twice', () => {
+        const checkoutService = setup();
+        const item1 = {
+          sku: 'atv',
+          name: 'Apple TV',
+          price: 10950,
+        };
+        const item2 = {
+          sku: 'vga',
+          name: 'VGA adapter',
+          price: 3000,
+        };
+        when(mockedProductRepository.findProductBySku(item1.sku)).thenReturn(item1);
+        when(mockedProductRepository.findProductBySku(item2.sku)).thenReturn(item2);
+        when(mockedProductConfigurationRepository.findBySku(item1.sku)).thenReturn({
+          skus: [item1.sku],
+          strategy: 'quantity',
+          config: {
+            quantityThreshold: 3,
+            discountedPrice: 10950,
+          },
+        });
+        checkoutService.scan(item1.sku); // 10950
+        checkoutService.scan(item1.sku); // 10950
+        checkoutService.scan(item1.sku); // Free
+        checkoutService.scan(item1.sku); // 10950
+        checkoutService.scan(item1.sku); // 10950
+        checkoutService.scan(item1.sku); // Free
+        checkoutService.scan(item1.sku); // 10950
+        checkoutService.scan(item2.sku); // 3000
+        verify(mockedProductRepository.findProductBySku(item1.sku)).times(7);
+        verify(mockedProductRepository.findProductBySku(item2.sku)).once();
+        expect(checkoutService.total()).toEqual(57750);
+      });
+
+      it('should apply no discount if productConfig not found', () => {
+        const checkoutService = setup();
+        const item1 = {
+          sku: 'atv',
+          name: 'Apple TV',
+          price: 10950,
+        };
+        const item2 = {
+          sku: 'vga',
+          name: 'VGA adapter',
+          price: 3000,
+        };
+        when(mockedProductRepository.findProductBySku(item1.sku)).thenReturn(item1);
+        when(mockedProductRepository.findProductBySku(item2.sku)).thenReturn(item2);
+        when(mockedProductConfigurationRepository.findBySku(item1.sku)).thenReturn(undefined);
+        checkoutService.scan(item1.sku);
+        checkoutService.scan(item2.sku);
+        verify(mockedProductRepository.findProductBySku(item1.sku)).once();
+        verify(mockedProductRepository.findProductBySku(item2.sku)).once();
+        expect(checkoutService.total()).toEqual(10950 + 3000);
+      });
+
+      it('should apply no discount if productConfig found but threshhold not reached', () => {
+        const checkoutService = setup();
+        const item1 = {
+          sku: 'atv',
+          name: 'Apple TV',
+          price: 10950,
+        };
+        const item2 = {
+          sku: 'vga',
+          name: 'VGA adapter',
+          price: 3000,
+        };
+        when(mockedProductRepository.findProductBySku(item1.sku)).thenReturn(item1);
+        when(mockedProductRepository.findProductBySku(item2.sku)).thenReturn(item2);
+        when(mockedProductConfigurationRepository.findBySku(item1.sku)).thenReturn({
+          skus: [item1.sku],
+          strategy: 'quantity',
+          config: {
+            quantityThreshold: 3,
+            discountedPrice: 10950,
+          },
+        });
+        checkoutService.scan(item1.sku);
+        checkoutService.scan(item1.sku);
+        verify(mockedProductRepository.findProductBySku(item1.sku)).twice();
+        expect(checkoutService.total()).toEqual(10950 + 10950);
+      });
+    });
   });
 });
